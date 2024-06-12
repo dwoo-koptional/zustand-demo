ec884991543214d6b73e2af34edb262bfbe6501e
dwoo-koptional <dwoo@koptional.com>
2024-06-12 11:15:08 -0400

    feat: implement zustand store and types

---
+++ src/hooks/useStore.tsx
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import { Todo } from "../types/Todo";
+
+export interface TodoState {
+  todos: Todo[];
+  addTodo: (todo: Todo) => void;
+  deleteTodo: (idx: number) => void;
+  updateTodo: (idx: number) => void;
+}
+
+export const useTodoStore = create<TodoState>((set) => {
+  return {
+    todos: [],
+    addTodo: (todo) =>
+      set(({ todos: prevTodos }) => ({ todos: [...prevTodos, todo] })),
+    deleteTodo: (idx) =>
+      set(({ todos: prevTodos }) => ({
+        todos: prevTodos.filter((_, index) => idx !== index),
+      })),
+    updateTodo: (idx) =>
+      set(({ todos: prevTodos }) => ({
+        todos: prevTodos.map((todo, index) =>
+          idx === index ? { ...todo, completed: !todo.completed } : todo
+        ),
+      })),
+  };
+});
